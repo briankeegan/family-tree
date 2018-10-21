aeb35682efca9e2b398e4e50ea67984b2bebb8a3
Unknown <brian.morris.keegan@gmail.com>
2018-10-21 18:47:35 -0400

    Create getObj, and getParents

--- src/js/processData.js
+++ src/js/processData.js
@@ -2,22 +2,21 @@ const processData = (
   { families },
   targetIds
 ) => {
-  // console.log(families)
   const getMember = ({ familyNameId, memberId }) => {
     const family = (families || []).find(family => family.familyNameId === familyNameId);
     const member = (family.members || []).find(member => member.memberId === memberId);
     return member || {};
   };
 
+  const getExistingMembers = members => (
+    members.filter(memberIds => getMember(memberIds).fullName)
+  );
 
   const getDepth = (targetIds, type) => {
     const getDepthRecursively = (targetIds) => {
       const member = getMember(targetIds);
       if ((member[type] || []).length) {
-        // Filter out members that have indexes, but are not on the tree
-        const existingMembers = member[type].filter(memberIds => {
-          return getMember(memberIds).fullName;
-        });
+        const existingMembers = getExistingMembers(member[type]);
         return Math.max(...existingMembers.map(getDepthRecursively)) + 1;
       }
       return 0;
@@ -31,10 +30,37 @@ const processData = (
   const parentDepth = Math.max(...[targetIds, ...target.partners]
     .map(ids => getDepth(ids, 'parents')));
 
-  console.log('childrenDepth', childrenDepth)
-  console.log('parentDepth', parentDepth)
-  console.log('target', target)
+  const depth = childrenDepth + parentDepth;
+  console.log('depth', depth)
+  const targetDepth = depth - parentDepth;
+  console.log('targetDepth', targetDepth)
 
+  const getParentsRecursively = (parentsIds) => {
+    const { fullName, parents } = getMember(parentsIds);
+    const parent = { fullName };
+    if ((parents || []).length) {
+      const existingParents = getExistingMembers(parents);
+      if (existingParents.length) {
+        parent.parents = existingParents.map(getParentsRecursively);
+      }
+    }
+    return parent;
+  };
+
+
+  const getFamilyObj = () => {
+    const existingPartner = getExistingMembers([targetIds, ...target.partners]);
+    return existingPartner.map(partnerIds => {
+      const { fullName, parents } = getMember(partnerIds);
+      const partner = { fullName };
+      if (parents) {
+        partner.parents = parents.map(getParentsRecursively)
+      }
+      return partner;
+    });
+  };
+
+  console.log('getFamilyObj(target)', getFamilyObj(target))
 };
 
 export default processData;
