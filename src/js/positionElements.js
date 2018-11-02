ed7e619a9ec46ac1766d54f72032762200eaffb4
Unknown <brian.morris.keegan@gmail.com>
2018-11-01 22:26:58 -0400

    WIP, but appears to be creating correct array

--- src/js/positionElements.js
+++ src/js/positionElements.js
@@ -29,16 +29,118 @@ const positionElements = (dimensions, svg, familyData, member) => {
     return getChildrenOffset(familyData, ids);
   };
 
+  const mutateOffset = (array, depth, index, offset) => {
+    let hasMutatedDepth = false;
+    for (let i = index; i < array[depth].length; i++) {
+      array[depth][i].offset += offset;
+      const { childRef } = array[depth][i];
+      if (!hasMutatedDepth && childRef) {
+        const { start } = childRef;
+        console.log('here!', depth + 1, start, offset)
+        mutateOffset(childrenArray, depth + 1, start, offset);
+        hasMutatedDepth = true;
+      }
+    }
+  };
+
+  const childrenArray = [];
 
   const renderChildren = (children, parentsPoint = 'preachy') => {
-    let curOffset = 0;
+    const createChildrenArray = (children, depth = 0) => {
+      (children || []).forEach((child, i) => {
+        if (!childrenArray[depth]) {
+          childrenArray[depth] = [];
+        }
+        if ((child.children || []).length) {
+          const { children } = child;
+          const childDepth = depth + 1;
+          const start = (childrenArray[depth + 1] || []).length;
+          const { length } = children;
+          const end = start + length;
+          child.childRef = { start, end };
+          createChildrenArray(children, childDepth);
+        }
+        childrenArray[depth].push(child);
+      });
+    };
+    createChildrenArray(children);
+
+    for (let depth = childrenArray.length - 1; depth >= 0; depth--) {
+      childrenArray[depth].forEach((child, i) => {
+        let initialOffset = i * 2;
+        if (depth === childrenArray.length -1) {
+          child.offset = initialOffset;
+        } else {
+          if ((child.children || []).length) {
+            const { childRef, children } = child;
+            const { start, end } = childRef;
+            const childAdjustedPosition = childrenArray[depth + 1]
+              .slice(start, end)
+              .reduce((num, grandChild) => {
+                // try refactoring to `return num += grandChild.offset;`
+                num += grandChild.offset;
+                return num;
+              }, 0) / children.length;
+            if (!i && childAdjustedPosition) {
+              mutateOffset(childrenArray, depth + 1, start, -childAdjustedPosition);
+            } else if (childAdjustedPosition < initialOffset) {
+              mutateOffset(childrenArray, depth + 1, start, initialOffset - childAdjustedPosition);
+            } else if (childAdjustedPosition > initialOffset) {
+              initialOffset = childAdjustedPosition;
+            }
+          }
+          child.offset = initialOffset;
+        }
+      });
+    }
+  };
+
+  const renderChildren321 = (children, parentsPoint = 'preachy') => {
+    const renderChildrenRecursively = (children, depth = 0) => {
+      (children || []).forEach((child, i) => {
+        let initialOffset = i * 2;
+        if (!childrenArray[depth]) {
+          childrenArray[depth] = [];
+        }
+        if ((child.children || []).length) {
+          const { children } = child;
+          const childDepth = depth + 1;
+          const start = (childrenArray[depth + 1] || []).length;
+          const { length } = children;
+          const end = start + length;
+          child.startChildRef = start;
+          renderChildrenRecursively(children, childDepth);
+          const childAdjustedPosition = childrenArray[childDepth]
+            .slice(start, end)
+            .reduce((num, grandChild) => {
+              // try refactoring to `return num += grandChild.offset;`
+              num += grandChild.offset;
+              return num;
+            }, 0) / length;
+          if (i && childAdjustedPosition) {
+            mutateOffset(childrenArray[childDepth], start, -childAdjustedPosition);
+          } else if (childAdjustedPosition < initialOffset) {
+            mutateOffset(childrenArray[childDepth], start, childAdjustedPosition - initialOffset);
+          }
+        }
+        const offset = childrenArray[depth].length ?
+          childrenArray[depth][childrenArray[depth].length -1].offset + 2 :
+          0;
+        child.offset = offset;
+        childrenArray[depth].push(child);
+      });
+    };
+    renderChildrenRecursively(children);
+  };
+
+  const renderChildren123 = (children, parentsPoint = 'preachy', depth = 0 ) => {
     const points = (children || []).map((child, i) => {
       const padding = boxWidth + childrenPadding;
       const x = parentsPoint[0];
       let offset = x - padding * (children.length - 1 + curOffset) / 2;
       const y = parentsPoint[1];
       let childrenOffset = _getChildrenOffset(child.ids);
-      if (childrenOffset) {
+      if (childrenOffset) {``
         curOffset -= childrenOffset * 2;
         offset += padding * childrenOffset / 2;
       }
@@ -115,6 +217,8 @@ const positionElements = (dimensions, svg, familyData, member) => {
   };
 
   createTarget();
+
+  console.log(childrenArray);
 
   lines.forEach(line => appendLine(line));
   textBoxes.forEach(textBox => {
