21984d4dcb6584d23dddacef4ea9d447a735554b
Brian Keegan <brian.morris.keegan@gmail.com>
2019-08-08 21:26:58 -0400

    a lil more refactoring
    I do not think that word means what you think it means

--- src/js/processData.js
+++ src/js/processData.js
@@ -38,52 +38,48 @@ const getChildrenRecursively = (childIds, families) => {
 }
 
 const processData = ({ families }, targetIds) => {
-  const getFamilyObj = () => {
-    const target = getMember(targetIds, families)
-    const { fullName, parents, children, memberId } = target
-    const targetMember = { fullName, memberId }
-    if (parents && parents.length) {
-      targetMember.parents = parents.map(parentIds =>
-        getParentsRecursively(parentIds, families)
-      )
-    }
-    if (children && children.length) {
-      targetMember.children = children.map(childIds =>
-        getChildrenRecursively(childIds, families)
-      )
-    }
-    const partners = getExistingMembers(target.partners, families).map(
-      partnerIds => {
-        const { fullName, parents, children, memberId } = getMember(
-          partnerIds,
-          families
-        )
-        const partner = { fullName, memberId, ids: partnerIds }
-        if (parents) {
-          partner.parents = parents.map(parentIds =>
-            getParentsRecursively(parentIds, families)
-          )
-        }
-        if (children && children.length) {
-          partner.children = children.map(child => {
-            const { memberId, fullName } = getMember(child, families)
-            return { memberId, fullName, ids: child }
-          })
-        }
-        return partner
-      }
+  const target = getMember(targetIds, families)
+  const { fullName, parents, children, memberId } = target
+  const targetMember = { fullName, memberId }
+  if (parents && parents.length) {
+    targetMember.parents = parents.map(parentIds =>
+      getParentsRecursively(parentIds, families)
     )
-    return [targetMember, ...partners]
   }
-
-  return getFamilyObj()
+  if (children && children.length) {
+    targetMember.children = children.map(childIds =>
+      getChildrenRecursively(childIds, families)
+    )
+  }
+  const partners = getExistingMembers(target.partners, families).map(
+    partnerIds => {
+      const { fullName, parents, children, memberId } = getMember(
+        partnerIds,
+        families
+      )
+      const partner = { fullName, memberId, ids: partnerIds }
+      if (parents) {
+        partner.parents = parents.map(parentIds =>
+          getParentsRecursively(parentIds, families)
+        )
+      }
+      if (children && children.length) {
+        partner.children = children.map(child => {
+          const { memberId, fullName } = getMember(child, families)
+          return { memberId, fullName, ids: child }
+        })
+      }
+      return partner
+    }
+  )
+  return [targetMember, ...partners]
 }
 
 // long term, should determine which children are shared... from which partners
-const isPartnerOfChildren = (member, partner) => {
-  return member.children.every((child, i) => {
-    return child.memberId === partner.children[i].memberId
-  })
-}
+// const isPartnerOfChildren = (member, partner) => {
+//   return member.children.every((child, i) => {
+//     return child.memberId === partner.children[i].memberId
+//   })
+// }
 
-export { processData, isPartnerOfChildren }
+export { processData }
